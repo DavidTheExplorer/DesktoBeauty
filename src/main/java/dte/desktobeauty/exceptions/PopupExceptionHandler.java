23390b8c6492536a1602b9a8b115cfae9019dc64
David <darknight4123@gmail.com>
2023-09-09 09:03:05 +0300

    Error popups now show the full stacktrace instead of just the message

--- src/main/java/dte/desktobeauty/exceptions/PopupExceptionHandler.java
+++ src/main/java/dte/desktobeauty/exceptions/PopupExceptionHandler.java
@@ -3,6 +3,8 @@ package dte.desktobeauty.exceptions;
 import java.io.IOException;
 import java.lang.Thread.UncaughtExceptionHandler;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
 import dte.desktobeauty.state.State;
 import dte.desktobeauty.utils.AlertUtils;
 
@@ -11,23 +13,22 @@ public class PopupExceptionHandler implements UncaughtExceptionHandler
 	@Override
 	public void uncaughtException(Thread thread, Throwable throwable)
 	{
-		String message = throwable.getMessage();
-		State state = State.current();
-
-		switch(state) 
+		String stackTrace = ExceptionUtils.getStackTrace(throwable);
+		
+		switch(State.current()) 
 		{
 		case RUNNING:
-			AlertUtils.error("Error while switching a Background Picture:", message);
+			AlertUtils.error("Error while switching a Background Picture:", stackTrace);
 			break;
 
 		case SETTING_SYSTEM_TRAY:
 			String action = (throwable instanceof IOException ? "reading" : "displaying");
 
-			AlertUtils.error(String.format("Error while %s the System-Tray's Image:", action), message);
+			AlertUtils.error(String.format("Error while %s the System-Tray's Image:", action), stackTrace);
 			break;
 
 		default:
-			AlertUtils.error(String.format("NO handler was defined to handle exceptions within the '%s' state:", state.name()), message);
+			AlertUtils.error(String.format("NO handler was defined to handle exceptions within the '%s' state:", State.current().name()), stackTrace);
 			break;
 		}
 
