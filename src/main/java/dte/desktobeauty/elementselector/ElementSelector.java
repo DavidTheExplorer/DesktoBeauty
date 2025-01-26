fa0c8c2cfdc08e85d8ebe244000a70222ab48715
David <darknight4123@gmail.com>
2025-01-26 05:45:57 +0200

    Small readability improvements

--- src/main/java/dte/desktobeauty/elementselector/ElementSelector.java
+++ src/main/java/dte/desktobeauty/elementselector/ElementSelector.java
@@ -1,7 +1,7 @@
 package dte.desktobeauty.elementselector;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * This interface represents logic of selecting an element from a {@link java.util.List}.
@@ -15,17 +15,16 @@ public interface ElementSelector<T>
 	
 	
 	/**
-	 * Returns an {@code ElemenetSelector} that corresponds to the provided {@code name}.
+	 * Returns an {@code ElementSelector} that corresponds to the provided {@code name}.
 	 * @param <T> The type of objects the selector should treat.
 	 * @param selectorName The name of the selector to search.
 	 * 
 	 * @return A selector whose name matches the provided name.
 	 * @throws IllegalArgumentException If no such selector was found.
 	 */
-	public static <T> ElementSelector<T> fromName(String selectorName)
+	static <T> ElementSelector<T> fromName(String selectorName)
 	{
-		return Arrays.asList(new RandomElementSelector<T>(), new RandomOrderSelector<T>())
-				.stream()
+		return Stream.of(new RandomElementSelector<T>(), new RandomOrderSelector<T>())
 				.filter(selector -> selector.getName().equalsIgnoreCase(selectorName))
 				.findFirst()
 				.orElseThrow(() -> new IllegalArgumentException(String.format("Couldn't find an element selector named '%s'!", selectorName)));
