c56c2c237a944ed73b7d0c0cc82b08bbc4ef4916
David <darknight4123@gmail.com>
2025-01-27 06:17:13 +0200

    Removed UnsupportedExtensionException
    Since exceptions are just displayed in a popup, there was no justification to not use IllegalArgumentException

--- src/main/java/dte/desktobeauty/desktop/DesktopPicture.java
+++ src/main/java/dte/desktobeauty/desktop/DesktopPicture.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import dte.desktobeauty.exceptions.UnsupportedExtensionException;
 import dte.desktobeauty.utils.FileUtils;
 
 public class DesktopPicture 
@@ -16,12 +15,12 @@ public class DesktopPicture
 	 * Sets a new desktop picture.
 	 * 
 	 * @param picturePath The path to the new background picture.
-	 * @throws UnsupportedExtensionException If the provided path's extension either doesn't represent a picture, or not supported.
+	 * @throws IllegalArgumentException If the provided path's extension either doesn't represent a picture, or not supported.
 	 */
-	public static void set(Path picturePath) throws UnsupportedExtensionException
+	public static void set(Path picturePath)
 	{
 		if(!isSupportedExtension(picturePath))
-			throw new UnsupportedExtensionException(FileUtils.getExtension(picturePath));
+			throw new IllegalArgumentException(String.format("Cannot set the desktop picture to a \"%s\" file.", FileUtils.getExtension(picturePath)));
 		
 		User32.INSTANCE.SystemParametersInfo(0x0014, 0, picturePath.toString(), 1);
 	}
